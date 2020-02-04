a6145fe0a08d38acbf29e3fb002f40e72ae6c6ff
Brian <brc@odoo.com>
2020-02-04 19:18:36 +0000

    model updated

---
+++ technical-training-13.0-01-models/openacademy/models/course.py
@@ -0,0 +1,9 @@
+from odoo import models, fields
+
+class OpenAcademyCourse(models.Model):
+    _name = "openacademy.course"
+    _description = "OpenAcademy Course"
+    name = fields.Char(string='Course Title', required=True, index=True,help='Enter your course title on this field.')
+    description = fields.Html(string='Description')
+    banner = fields.Binary(string='Banner')
+    pride = foe;ds.Float(string='Price',digits=(5,4))
